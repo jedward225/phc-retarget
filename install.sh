6160692f0a33bb6bf87c7e449f849f3ba7b56a70
liucao <l-cao20@mails.tsinghua.edu.cn>
2025-09-24 13:38:55 +0800

    dependencies

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 uv venv --python 3.8 .venv
 source .venv/bin/activate
-uv pip install -U pip setuptools wheel
+uv pip install -U pip setuptools wheel gdown
 uv pip install numpy==1.23.5 scipy open3d
 uv pip install torch torchvision --torch-backend=cu118
 uv pip install --no-build-isolation "chumpy==0.70"
